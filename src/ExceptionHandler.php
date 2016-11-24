40c6ab65f1309dedbee5020d56caf4321875948a
Kit-kat1 <Katynichka95@mail.ru>
2016-11-24 13:01:20 +0200

    fix exception handler

--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -57,6 +57,22 @@ class ExceptionHandler extends Handler
         $this->config = $container->config->get('exceptions', []);
         $this->container = $container;
 
-        parent::__construct($container->make(LoggerInterface::class));
+        parent::__construct(app());
+    }
+
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+
+        return redirect()->guest('login');
     }
 }
